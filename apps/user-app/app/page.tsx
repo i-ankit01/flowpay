54a7270386b0b75c90498a8d3d9f3e823a1c3c17
i-ankit01 <ankit.common.acc@gmail.com>
2025-10-04 21:12:26 +0530

    init prisma

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Card } from "@repo/ui/card";
+import { Gradient } from "@repo/ui/gradient";
+import { TurborepoLogo } from "@repo/ui/turborepo-logo";
+
+
+export default function Page() {
+  return (
+    <div className="text-2xl">
+      Hi Ankit
+    </div>
+  );
+}
