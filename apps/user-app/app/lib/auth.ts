5c9a39b81a477c1ae2a3b49567257506ceadd2ba
i-ankit01 <ankit.common.acc@gmail.com>
2025-10-09 01:50:37 +0530

    update auth

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -1,3 +1,5 @@
+import db from '@repo/db/client';
+import  bcrypt from 'bcrypt';
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 // import { signIn } from "next-auth/react";
@@ -9,33 +11,57 @@ export const authOptions = {
     CredentialsProvider({
       name: "Credentials", // this is written in the button "Sign In with Credentials"
       credentials: {
-        username: { label: "email", type: "text", placeholder: "Enter Email" },
-        password: {
-          label: "password",
-          type: "password",
-          placeholder: "Enter Password",
-        },
-      },
-      async authorize(credentials: any) {
-        // Do the database checks and validations here
-        const username = credentials.username;
-        const password = credentials.password;
+            phone: { label: "Phone number", type: "text", placeholder: "1231231231", required: true },
+            password: { label: "Password", type: "password", required: true }
+          },
+      // TODO: User credentials type from next-aut
+          async authorize(credentials: any) {
+            // Do zod validation, OTP validation here
+            const hashedPassword = await bcrypt.hash(credentials.password, 10);
+            const existingUser = await db.user.findFirst({
+                where: {
+                    number: credentials.phone
+                }
+            });
 
-        // This is supposed to be returned by the database
-        return {
-          id: "user1",
-          name: "Ankit",
-          userId: "ankit123",
-          email: "ankit@gmail.com",
-        };
-      },
+            if (existingUser) {
+                const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
+                if (passwordValidation) {
+                    return {
+                        id: existingUser.id.toString(),
+                        name: existingUser.name,
+                        email: existingUser.number
+                    }
+                }
+                return null;
+            }
+
+            try {
+                const user = await db.user.create({
+                    data: {
+                        number: credentials.phone,
+                        password: hashedPassword
+                    }
+                });
+            
+                return {
+                    id: user.id.toString(),
+                    name: user.name,
+                    email: user.number
+                }
+            } catch(e) {
+                console.error(e);
+            }
+
+            return null
+          },
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET || "secret",
   callbacks: {
     // The jwt callback is used when you want to change something and embedd in jwt
 
